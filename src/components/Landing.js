376283da3f01cd410225e880f3d25b99d660e0db
Josh Anderson <joshua.frederick.anderson@gmail.com>
2018-12-04 15:04:41 -0800

    created routes and components

---
+++ src/components/Landing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Landing = () => (
+  <section className="landing">
+    Landing page will go here
+);
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Library = () => (
+  <section className="library">
+    Landing page will go here
+);
+
+export default Library;
