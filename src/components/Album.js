df076d4de9cacb705150619fb9fd8eac72a400b2
Josh Anderson <joshua.frederick.anderson@gmail.com>
2018-12-12 20:23:58 -0800

    Added audio playback

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,9 +10,40 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
+
+  play(){
+    this.audioElement.play();
+    this.setState({ isPlaying: true});
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false});
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+  }
+
   render() {
     const { album } = this.state;
 
@@ -35,7 +66,7 @@ class Album extends Component {
           <tbody>
           {
             album.songs.map( (song, index) =>
-            <div key={index} >
+            <div className="song" key={index} onClick={() => this.handleSongClick(song)}  >
             <tr>{index + 1}. {song.title} {song.duration}</tr>
             </div>
             )
