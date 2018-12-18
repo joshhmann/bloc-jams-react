b64a0479dabaf267f7b68dccb528d7cfe9c56f56
Josh Anderson <joshua.frederick.anderson@gmail.com>
2018-12-18 00:53:59 -0800

    added play/pause icons

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,7 +12,8 @@ class Album extends Component {
     this.state = {
       album: album,
       currentSong: album.songs[0],
-      isPlaying: false
+      isPlaying: false,
+      isHovered: false
     };
 
     this.audioElement = document.createElement('audio');
@@ -21,7 +22,12 @@ class Album extends Component {
 
     play(){
       this.audioElement.play();
-      this.setState({ isPlaying: true});
+      this.setState({ isPlaying: true,
+      songNumber: 'icon ion-ios-play'
+
+      });
+
+
     }
 
     pause() {
@@ -77,13 +83,21 @@ class Album extends Component {
           <tbody>
           {
             album.songs.map( (song, index) =>
-            <tr className="song" key={index} onClick={() => this.handleSongClick(song)}  >
+            <tr className="song" key={index} onClick={() => this.handleSongClick(song)}
+            onMouseEnter={() => this.setState({isHovered: index+1})}
+            onMouseLeave={() => this.setState({isHovered: false})}
+            >
               <td className= "song-actions">
-                <button>
-                  <span className="song-number">{index + 1}</span>
-                  <span className="ion-play"></span>
-                  <span className="ion-pause"></span>
-                </button>
+              <button id="song-action-btns">
+              { (this.state.currentSong.title === song.title) ?
+                <span className={this.state.isPlaying ? "ion-pause" : "ion-play"}></span>
+                :
+                (this.state.isHovered === index+1) ?
+                <span className="ion-play"></span>
+                :
+                <span className="song-number">{index+1}</span>
+              }
+              </button>
               </td>
               <td className="song-title">{song.title}</td>
               <td className="song-duration">{song.duration}</td>
